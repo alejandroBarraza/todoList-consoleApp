73973b9f8150a571490aadfbade7c6cee3d70a38
alejandro <alejandro.barraza@alumnos.ucn.cl>
2021-07-25 23:39:24 -0400

    method arr to object

--- helpers/models/Tasks.js
+++ helpers/models/Tasks.js
@@ -4,9 +4,21 @@ class Tasks {
     constructor() {
         this._listTask = {};
     }
+    //create a new task
     createTask(taskToCreate) {
         const task = new Task(taskToCreate);
         this._listTask[task.id] = task;
+    }
+    //transfomr arr task to object task
+    arrToObject(arrTasks = []) {
+        arrTasks.forEach((task) => {
+            this._listTask[task.id] = task;
+        });
+    }
+    //return array of tasks
+    get toArray() {
+        // trasnform list tasks object in a array of tasks
+        return Object.values(this._listTask);
     }
 }
 module.exports = Tasks;
